09c122a4e93a234f34890a5ba695be30c168910b
Maryanemwende <maryanemwende333@gmail.com>
2023-11-29 08:19:00 -0800

    all tasks correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #Using Puppet, create a file in /tmp
 
 file {'/tmp/school':
-ensure => present,
-mode => '0744',
+ensure  => present,
+mode    => '0744',
 content => 'I love Puppet',
-owner => 'www-data',
-group => 'www-data',
+owner   => 'www-data',
+group   => 'www-data',
 }
